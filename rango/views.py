9222a428993db67d43d19d04aa08774b9434a552
dominik791 <dominik.broj@gmail.com>
2015-05-27 21:24:26 +0200

    add about page

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return "Rango says HEELLOO!!!"
+    return HttpResponse("Rango says HEELLOO!!! <br/> <a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("Rango says this is the about page. <a href='/rango/'>Index</a>")
