1c6b09de41dcd8dceb2aa5fb41162e157d2e8a0a
dominik791 <dominik.broj@gmail.com>
2015-05-28 20:39:35 +0200

    add a template

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -25,6 +26,12 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
+
+TEMPLATE_DIRS = (
+    #"tango_with_django_project/templates"
+    TEMPLATE_PATH,
+    )
+
 
 
 # Application definition
