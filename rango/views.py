188acee51822ccdaa70500115655cf4c9e4c67f8
dominik791 <dominik.broj@gmail.com>
2015-05-30 00:29:07 +0200

    add about.html template to the about page

--- rango/views.py
+++ rango/views.py
@@ -8,4 +8,4 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("Rango says this is the about page. <a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
