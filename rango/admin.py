e832bc761e9620aed565da8fc572a283401fac7c
dominik791 <dominik.broj@gmail.com>
2015-06-01 16:44:05 +0200

    Customise Admin Interface

--- rango/admin.py
+++ rango/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from rango.models import Category, Page
 # Register your models here.
 
+    list_display = ['title', 'category', 'url']
+
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
