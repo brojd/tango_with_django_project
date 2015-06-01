2e4b18a7f944a638253791968c0ea26055c0655c
dominik791 <dominik.broj@gmail.com>
2015-06-01 18:59:13 +0200

    create category view

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 
 def index(request):
@@ -10,3 +11,15 @@ def index(request):
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'rango/category.html', context_dict)
