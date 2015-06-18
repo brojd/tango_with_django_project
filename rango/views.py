f41c6120bf26488c5a15602ea29dd745daecd755
dominik791 <dominik.broj@gmail.com>
2015-06-18 23:09:51 +0200

    Creating an Add Pages View, Template and URL Mapping

--- rango/views.py
+++ rango/views.py
@@ -7,7 +7,7 @@ from rango.forms import CategoryForm, PageForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('-views')[:5]
+    page_list = Page.objects.order_by('-views')[:20]
     context_dict = {
         'categories': category_list,
         'pages': page_list,
@@ -23,6 +23,7 @@ def category(request, category_name_slug):
     context_dict = {}
     try:
         category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name_slug'] = category_name_slug
         context_dict['category_name'] = category.name
         pages = Page.objects.filter(category=category)
         context_dict['pages'] = pages
@@ -47,3 +48,32 @@ def add_category(request):
         form = CategoryForm()
     
     return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        cat = None
+    
+    if request.method == "POST":
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                return category(request, category_name_slug)
+            
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+    
+    context_dict = {'form': form, 'category': cat, 'category_name_slug': category_name_slug}
+    
+    return render(request, 'rango/add_page.html', context_dict)
+                
+                
+        
